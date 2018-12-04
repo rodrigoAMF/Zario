d9d1307cd670f1f3e80a01de12a260f1da579f64
Rodrigo Franco <rodrigoAMF@outlook.com>
2018-12-04 00:06:24 -0200

    Adicionado sprites, animação dos zombies. Criado prefab zombieMale
    Falta programar o script do inimigo

---
+++ Assets/Scripts/EnemyHorizontal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHorizontal : MonoBehaviour {
+    private float time;
+
+    public float timer;
+    public float movSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+        time = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
