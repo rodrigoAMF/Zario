de59ce5eb1e9b9c1fd84826831fed88f83a560bc
Rodrigo Franco <rodrigoAMF@outlook.com>
2018-12-04 23:03:20 -0200

    Adicionado lógica para morrer, perder vida. closes #1
    Removido barra de força

--- Assets/Scripts/EnemyHorizontal.cs
+++ Assets/Scripts/EnemyHorizontal.cs
@@ -3,19 +3,43 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHorizontal : MonoBehaviour {
-    private float time;
+    public bool collide;
+    Rigidbody2D rb;
 
-    public float timer;
     public float movSpeed;
 
 
 	// Use this for initialization
 	void Start () {
-        time = 0.0f;
+        rb = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        
+        rb.velocity = new Vector2(movSpeed, rb.velocity.y);
+        if (collide) {
+            flip();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemyCollider")) {
+            collide = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("enemyCollider")) {
+            collide = false;
+        }
+    }
+
+    private void flip()
+    {
+        GetComponent<SpriteRenderer>().flipX = !GetComponent<SpriteRenderer>().flipX;
+        movSpeed *= -1;
+        collide = false;
     }
 }
