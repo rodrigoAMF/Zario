87d8623545949a8ba3e28f32b333a5271522f672
Rodrigo Franco <rodrigoAMF@outlook.com>
2018-12-04 23:03:20 -0200

    Adicionado lógica para morrer, perder vida e corigido issue #1
    Removido barra de força

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,26 +4,36 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
-    private int numeroVidas;
     private int forcaPulo;
     private float movimento;
     private int moedas;
     private float velocidadeMaxima;
     private bool estaNoChao;
+    private int vidaAtual;
+    private int numeroVidas;
+
+    private double tamanhoMaxBarraVida;
+    private double tamanhoMaxBarraForca;
+
     private Rigidbody2D rb;
     private Animator animator;
     public Text lblNumeroMoedas;
     public Text lblNumeroVidas;
+    public GameObject barraVidaAtual;
 
 	// Use this for initialization
 	void Start () {
+        tamanhoMaxBarraVida = 21;
+        tamanhoMaxBarraForca = 21;
         forcaPulo = 200;
         velocidadeMaxima = 2;
         moedas = 0;
         numeroVidas = 3;
+        vidaAtual = 100;
         rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         lblNumeroVidas.text = numeroVidas.ToString();
+        GetComponent<Transform>().position = GameObject.Find("LevelBegin").GetComponent<Transform>().position;
     }
 	
 	// Update is called once per frame
@@ -70,12 +80,32 @@ public class Player : MonoBehaviour {
             moedas++;
             lblNumeroMoedas.text = moedas.ToString();
         }
+
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Plataformas")){
             estaNoChao = true;
+        }
+        if (collision.gameObject.CompareTag("Enemy")) {
+            vidaAtual -= 20;
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(-500, 500));
+            Vector3 tamanhoAtual = barraVidaAtual.GetComponent<Transform>().localScale;
+            Vector3 novoTamanho = new Vector3((float)(tamanhoMaxBarraVida * (vidaAtual / 100.0)), tamanhoAtual.y, tamanhoAtual.z);
+            barraVidaAtual.GetComponent<Transform>().localScale = novoTamanho;
+            if (vidaAtual <= 0) {
+                numeroVidas--;
+                lblNumeroVidas.text = numeroVidas.ToString();
+                moedas = 0;
+                lblNumeroMoedas.text = moedas.ToString();
+                vidaAtual = 100;
+                barraVidaAtual.GetComponent<Transform>().localScale = new Vector3((float)tamanhoMaxBarraVida, tamanhoAtual.y, tamanhoAtual.z);
+                GetComponent<Transform>().position = GameObject.Find("LevelBegin").GetComponent<Transform>().position;
+                if (numeroVidas == 0) {
+                    // Game Over
+                }
+            }
         }
     }
 
