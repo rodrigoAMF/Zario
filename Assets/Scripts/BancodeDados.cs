507511e9703c1947d165955c91be736fe0e8f945
luziane de lourdes freitas <freitas.lu@outlook.com>
2018-12-06 16:26:41 -0200

    Telas de  interface com usuario

---
+++ Assets/Scripts/BancodeDados.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BancodeDados : MonoBehaviour {
+    /*private static ArrayList<Usuario> usuario = new ArrayList();
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public static void setUsuario(ArrayList<Usuario> usuario)
+    {
+        BancodeDados.usuario = usuario;
+    }
+
+    public static ArrayList<Usuario> getUsuario()
+    {
+        return usuario;
+    }*/
+}
